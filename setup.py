1a37a72d5efa1dbfea0fa26e23bc9c0678005703
chpark <cksgusekdp@naver.com>
2021-03-17 15:49:08 +0900

    test

--- setup.py
+++ setup.py
@@ -18,9 +18,5 @@ setup(
     ],
     packages=['pandas_tfrecords'],
     install_requires=[
-        's3fs==0.4.0',
-        'tensorflow==2.1.0',
-        'pandas==1.0.1',
-        'numpy==1.18.1',
     ],
 )
